4fa8e3ef517945d9e8fe35aa2363eb61c27dac75
sergio2308 <serg2308n@yandex.ru>
2024-07-15 00:18:49 +0300

    style: поправить оформление

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -1,11 +1,11 @@
 package manager;
 
-public class Node<Task> {
-    public Task task;
-    public Node<Task> next;
-    public Node<Task> prev;
+public class Node<E> {
+    public E task;
+    public Node<E> next;
+    public Node<E> prev;
 
-    public Node(Task task, Node<Task> next, Node<Task> prev) {
+    public Node(E task, Node<E> next, Node<E> prev) {
         this.task = task;
         this.next = next;
         this.prev = prev;
