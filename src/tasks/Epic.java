9183d7aea8462f5321805f3615795c468dcc104c
sergio2308 <serg2308n@yandex.ru>
2024-07-15 00:06:50 +0300

    style: поправить оформление

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -2,7 +2,7 @@ package tasks;
 
 import java.util.ArrayList;
 
-public class Epic extends Task{
+public class Epic extends Task {
     protected ArrayList<Integer> subTasksIds;
 
     public Epic(String name, String description) {
