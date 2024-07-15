e74807350990566456c88b62d06d2ac440e30c33
sergio2308 <serg2308n@yandex.ru>
2024-07-15 12:39:38 +0300

    style: поправить оформление

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -5,9 +5,10 @@ import tasks.Task;
 import java.util.List;
 
 public interface HistoryManager {
+    
     void addHistory(Task task);
 
     void remove(int id);
-    
+
     List<Task> getHistory();
 }
